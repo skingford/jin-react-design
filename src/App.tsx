0e80dbcd0ad9863c9ee265735bae6a5159c07447
kingford <332312568@qq.com>
2021-05-17 01:53:55 +0800

    feat: 按钮组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
 /*
  * @Author: kingford
  * @Date: 2021-05-14 22:19:53
- * @LastEditTime: 2021-05-14 22:51:14
+ * @LastEditTime: 2021-05-17 01:37:02
  */
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from './components/Button'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-          Hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button >Hello</Button>
+
     </div>
   );
 }

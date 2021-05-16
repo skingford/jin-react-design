7edf9a1f36c38b61aebe1e523fdd3d56c31fc06b
kingford <332312568@qq.com>
2021-05-16 23:27:53 +0800

    ci: 项目初始化

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+/*
+ * @Author: kingford
+ * @Date: 2021-05-14 22:19:53
+ * @LastEditTime: 2021-05-14 22:51:14
+ */
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+          Hello
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
